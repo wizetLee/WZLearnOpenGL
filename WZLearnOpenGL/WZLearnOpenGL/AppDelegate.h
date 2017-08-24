48ae4df5d3f4a961925581805a6747ee94a9051a
admin <admin@admins-iMac-3.local>
2017-08-24 19:33:27 +0800

    Learn

---
+++ WZLearnOpenGL/WZLearnOpenGL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZLearnOpenGL
+//
+//  Created by admin on 22/8/17.
+//  Copyright © 2017年 wizet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WZLearnOpenGL/WZLearnOpenGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WZLearnOpenGL
+//
+//  Created by admin on 22/8/17.
+//  Copyright © 2017年 wizet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
